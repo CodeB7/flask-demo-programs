d9680c09f182ad664cf1116c83791da7ae6fea8b
GauravPK <gaurav.prabhu7@gmail.com>
2020-10-17 20:17:44 +0530

    Demo programs

--- src/_2_render_raw_html/app.py
+++ src/_2_render_raw_html/app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<center><h1>Sakkadankai namaskaru 🙏!<h1><h2>Flask shikkun kushi jalle!<h2><center>'
+    return '<center><h1>Sakkad jananka namaskaru 🙏!<h1><h2>Flask shikkuya!<h2><center>'
 
 if __name__ == '__main__':
-	# Try running this code by commentig this part!
     app.run(debug = True)
