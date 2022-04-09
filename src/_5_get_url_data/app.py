7c5292e1233be0496e348c83e86dd0a78b0acc0e
Gaurav <gaurav.prabhu7@gmail.com>
2022-04-09 16:09:29 +0530

    Changes

--- src/_5_get_url_data/app.py
+++ src/_5_get_url_data/app.py
@@ -4,10 +4,16 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/hello')
-@app.route('/hello/<name>')
+@app.route('/hello/')
+@app.route('/hello/<name>/')
 def index(name=''):
+    if not name:
+        return "<center><h1>Hi There!</h1></center>"
     return render_template('hello.html', name=name)
+
+@app.route('/')
+def default():
+    return "<a href='/hello'>Click Here</a>"
 
 if __name__ == '__main__':
     app.run(debug = True)
