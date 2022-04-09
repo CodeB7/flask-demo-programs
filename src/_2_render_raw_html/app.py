0b5fe8ff999f0a90eba5db872862a4d88cb72bcf
Gaurav <gaurav.prabhu7@gmail.com>
2022-04-09 15:55:36 +0530

    Changing message

--- src/_2_render_raw_html/app.py
+++ src/_2_render_raw_html/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<center><h1>Sakkad jananka namaskaru 🙏!<h1><h2>Flask shikkuya!<h2><center>'
+    return '<center><h1>Namaste Everyone! 🙏<h1><h2>Let\'s learn Flask!<h2><center>'
 
 if __name__ == '__main__':
     app.run(debug = True)
