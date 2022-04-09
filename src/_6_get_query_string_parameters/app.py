dcdff0a90116110481a067bf559fa87c4e18be98
Gaurav <gaurav.prabhu7@gmail.com>
2022-04-09 16:55:18 +0530

    Changes

--- src/_6_get_query_string_parameters/app.py
+++ src/_6_get_query_string_parameters/app.py
@@ -4,14 +4,22 @@ from flask import render_template, request
 app = Flask(__name__)
 
 
-@app.route('/hello/', methods=['GET'])
+# @app.route('/hello/', methods=['GET'])
+# def index():
+# 	first_name = request.args['fname']
+# 	last_name = request.args['lname']
+# 	return render_template('hello.html', fname=first_name, lname=last_name)
+
+@app.route('/form_submission', methods=['GET'])
+def form_submission():
+	first_name = request.args['first_name']
+	last_name = request.args['last_name']
+	return render_template('success.html', first_name=first_name, last_name=last_name)
+
 @app.route('/')
-def index():
-	if not request.args:
-		return "<h2>Please send First and Last Name in the query string.</h2>"
-	first_name = request.args['fname']
-	last_name = request.args['lname']
-	return render_template('hello.html', fname=first_name, lname=last_name)
+def home():
+	return render_template('form.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
